1cbb09b80a96c8d323d78ee4a4829b7e3ceb91b8
webBasedBob <catalin.b.pop97@gmail.com>
2022-11-26 22:29:55 +0200

    the first draft code for recipe search feature

--- recipeDirectory.js
+++ recipeDirectory.js
@@ -82,28 +82,117 @@ heroSectionTextBox.addEventListener("click", function (e) {
 //https://api.edamam.com/api/recipes/v2
 // ("http://www.edamam.com/ontologies/edamam.owl#recipe_");
 
-// to do
-//function to create url for fetch request - this is to make after all teh search fields are created
-//fn to make the request and rerturn the result
-// const recipeSearch = async function (url) {
-//   const rawResult = await fetch(url);
-//   const jsonResult = await rawResult.json();
-//   let recipesArr = jsonResult.hits.map((result) => result.recipe);
-//   return Promise.resolve(recipesArr);
-// };
-//fn to take the result and render it on the page
-// const renderResults = function (recipesArr) {
-//   const recipeResultsContainer = document.querySelector(".recipe-results");
-//   recipesArr.forEach((recipe) => {
-//     const htmmlToInsert = `<div class="recipe-result">
-//     <img src="${recipe.images.REGULAR.url}"/>
-//     <p class="recipe-result-title">${recipe.label}</p>
-//   </div>`;
-//     recipeResultsContainer.insertAdjacentHTML("afterbegin", htmmlToInsert);
-//   });
-// };
-//fn to expand the recipe
-//fn to save the recipe to favorites - maybe
 // console.log(recipeSearch(url));
 
 // recipeSearch(url).then((data) => renderResults(data));
+
+// recipes of the month carousel
+// consst recipesOfTheMonth = document.querySelectorAll(".carousel-item");
+// recipesOfTheMonth.forEach(
+//   (recipe, i) => (recipe.style.transform = `translateX(${i * 110}%)`)
+// );
+// let currentItem = 0;
+
+// const putCarouselItemsInPosition = function (itemsClass) {
+//   const carouselItems = document.querySelectorAll(`.${itemsClass}`);
+//   // if (currentItem === carouselItems.length - 2) currentItem = 0;
+//   // if (currentItem === 0) currentItem = carouselItems.length - 3;
+//   carouselItems.forEach((item, index) => {
+//     item.style.transform = `translateX(${(index - currentItem) * 110}%)`;
+//   });
+// };
+
+let searchResults = [];
+const correctUserInputText = function (str) {
+  const lowerCaseTrimmedStr = str.trim().toLowerCase();
+  const charArrWithAllSpaces = lowerCaseTrimmedStr
+    .split("")
+    .filter((char, index) => {
+      if (
+        lowerCaseTrimmedStr.charCodeAt(index) >= 97 &&
+        lowerCaseTrimmedStr.charCodeAt(index) <= 122
+      )
+        return true;
+      if (char === " ") return true;
+      return false;
+    });
+  const trimmedCharArr = charArrWithAllSpaces.filter((char, index) => {
+    if (char !== " ") return true;
+    if (charArrWithAllSpaces[index - 1] == " ") return false;
+    return true;
+  });
+  return trimmedCharArr.join("");
+};
+
+const createUrl = function () {
+  let resultUrl = [`https://api.edamam.com/api/recipes/v2?type=any`];
+  const appId = "a5cea2be";
+  const appKey = "95cea576a8a53c23997c5ec6c40084b7";
+  let userImputKeywords;
+  const searchInputField = document.querySelector(".main-search-field");
+  const dietFilter = document.querySelector(".diet-carousel-item.selected");
+  const cuisineFilter = document.querySelector(
+    ".cuisine-carousel-item.selected"
+  );
+  const mealTypeFilter = document.querySelector(".meal-type.selected");
+
+  userImputKeywords = correctUserInputText(searchInputField.value);
+
+  userImputKeywords &&
+    resultUrl.push(
+      `q=${userImputKeywords
+        .split(" ")
+        .reduce((acc, curVal) => acc + "%20" + curVal)}`
+    );
+  dietFilter && resultUrl.push(`health=${dietFilter.dataset.diet}`);
+  cuisineFilter && result.push(`cuisine=${cuisineFilter.dataset.cuisinetype}`);
+  mealTypeFilter &&
+    resultUrl.push(`mealType=${mealTypeFilter.dataset.mealtype}`);
+  resultUrl.push(`app_id=${appId}&app_key=${appKey}`);
+
+  return resultUrl.join("&");
+};
+
+const extractRecipeId = function (recipe) {
+  const id = recipe.uri.slice(recipe.uri.indexOf("recipe_") + 7);
+  return id;
+};
+
+const retreiveRecipesFromApi = async function (url) {
+  const rawResult = await fetch(url);
+  const jsonResult = await rawResult.json();
+  let recipesArr = jsonResult.hits.map((result) => result.recipe);
+  return recipesArr;
+};
+
+const storeRecipesGlobally = function (recipesArr) {
+  searchResults = recipesArr.map((recipe) => {
+    return { ...recipe, id: extractRecipeId(recipe) };
+  });
+};
+
+const renderResults = function (recipesArr) {
+  const recipeResultsContainer = document.querySelector(".recipe-results");
+  recipesArr.forEach((recipe) => {
+    const htmmlToInsert = `<div data-recipeid = "${extractRecipeId(
+      recipe
+    )}" class="recipe-result">
+    <img src="${recipe.images.REGULAR.url}"/>
+    <p class="recipe-result-title">${recipe.label}</p>
+  </div>`;
+    recipeResultsContainer.insertAdjacentHTML("afterbegin", htmmlToInsert);
+  });
+};
+
+const recipeSearch = async function () {
+  const url = createUrl();
+  const recipesArray = await retreiveRecipesFromApi(url);
+  storeRecipesGlobally(recipesArray);
+  console.log(recipesArray);
+  renderResults(recipesArray);
+};
+
+document.querySelector(".btn-search").addEventListener("click", recipeSearch);
+
+//fn to expand the recipe
+//fn to save the recipe to favorites - maybe

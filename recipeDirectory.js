5d677ef16d00b791be917d962f6853f309cc796b
webBasedBob <catalin.b.pop97@gmail.com>
2022-11-28 20:54:49 +0200

    created the functionality for the carousels on the page

--- recipeDirectory.js
+++ recipeDirectory.js
@@ -1,106 +1,123 @@
-// making the nav sticky
-const stickyNav = function (entries) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) body.classList.add("sticky");
-  if (entry.isIntersecting) body.classList.remove("sticky");
-};
-
-const body = document.querySelector("body");
-const heroSection = document.querySelector(".section-hero");
-
-const navObserver = new IntersectionObserver(stickyNav, {
-  root: null,
-  threshold: 0.12,
-});
-
-navObserver.observe(heroSection);
-// smooth scroling - used event delegation 😉
-const heroSectionTextBox = document.querySelector(".hero-text-box");
-heroSectionTextBox.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log(e.target);
-  if (e.target.tagName == "A") {
-    const scrollTo = document.querySelector(e.target.getAttribute("href"));
-    scrollTo.scrollIntoView({ behavior: "smooth" });
-  }
-});
-
-// carousel
-// const carousel = document.querySelector(".carousel");
-// const carouselItems = document.querySelectorAll(".carousel-item");
-// const carouselRightArr = document.querySelector(".carousel-arrow.arrow-right");
-// const carouselLeftArr = document.querySelector(".carousel-arrow.arrow-left");
-// let currentItem = 0;
-// const totalItems = carouselItems.length;
-// const resetCarousel = function () {
-//   carousel.style.display = "block";
-//   carousel.style.height = "600px";
-//   carousel.style.overflow = "hidden";
-//   carousel.style.position = "relative";
-//   carouselItems.forEach((el, i) => {
-//     el.style.position = "absolute";
-//     el.style.transform = `translateX(${i * 100}%)`;
-//     el.style.padding = "0 20px";
-//     el.style.transition = "all 0.3s";
-//   });
+// // making the nav sticky
+// const stickyNav = function (entries) {
+//   const [entry] = entries;
+//   if (!entry.isIntersecting) body.classList.add("sticky");
+//   if (entry.isIntersecting) body.classList.remove("sticky");
 // };
-// resetCarousel();
-// carouselRightArr.addEventListener("click", function () {
-//   if (currentItem === totalItems - 3) currentItem = 0;
-//   else currentItem++;
-//   carouselItems.forEach((el, i) => {
-//     el.style.transform = `translateX(${(i - currentItem) * 100}%)`;
-//   });
+
+// const body = document.querySelector("body");
+// const heroSection = document.querySelector(".section-hero");
+
+// const navObserver = new IntersectionObserver(stickyNav, {
+//   root: null,
+//   threshold: 0.12,
 // });
 
-// const [firstItem] = carouselItems;
-// firstItem.style.transform = "translate(114.6%, 0px)";
-// foodish api - images only - not the pretiest, but ok
-// fetch(`https://foodish-api.herokuapp.com/api`)
-//   .then((caca) => caca.json())
-//   .then((response) => {
-//     const img = document.querySelector(".hero-img");
-//     img.src = response.image;
-//   });
-// const appId = "a5cea2be";
-// const key = "95cea576a8a53c23997c5ec6c40084b7";
-// fetch(
-//   `https://api.edamam.com/api/recipes/v2?type=any&q=chicken&app_id=a5cea2be&app_key=95cea576a8a53c23997c5ec6c40084b7&imageSize=SMALL&random=true&
-//   `
-// )
-//   .then((caca) => caca.json())
-//   .then((response) => {
-//     const img = document.querySelector(".hero-img");
-//     img.src = response.hits[1].recipe.image;
-//     console.log(response);
-//   });
+// navObserver.observe(heroSection);
+// // smooth scroling - used event delegation 😉
+// const heroSectionTextBox = document.querySelector(".hero-text-box");
+// heroSectionTextBox.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   console.log(e.target);
+//   if (e.target.tagName == "A") {
+//     const scrollTo = document.querySelector(e.target.getAttribute("href"));
+//     scrollTo.scrollIntoView({ behavior: "smooth" });
+//   }
+// });
 
-// const url = `https://api.edamam.com/api/recipes/v2?type=any&q=chicken&app_id=a5cea2be&app_key=95cea576a8a53c23997c5ec6c40084b7&imageSize=SMALL&random=true&
-//   `;
-//
-//
-//https://api.edamam.com/api/recipes/v2
-// ("http://www.edamam.com/ontologies/edamam.owl#recipe_");
+const enableCarouselFunctionality = function (
+  carouselContainerClass,
+  carouselItemsClass,
+  prevBtnClass,
+  nextBtnClass
+) {
+  const carouselItems = document.querySelectorAll(`.${carouselItemsClass}`);
+  const nextBtn = document.querySelector(`.${nextBtnClass}`);
+  const prevBtn = document.querySelector(`.${prevBtnClass}`);
+  const carouselContainer = document.querySelector(
+    `.${carouselContainerClass}`
+  );
 
-// console.log(recipeSearch(url));
+  const getElmWidth = function (elm) {
+    return Number(
+      window.getComputedStyle(elm).getPropertyValue("width").slice(0, -2)
+    );
+  };
 
-// recipeSearch(url).then((data) => renderResults(data));
+  const getCarouselElmsWidthMean = function (elmsClass) {
+    const carouselElms = document.querySelectorAll(`.${elmsClass}`);
+    let elmsWidthSum = 0;
+    let noOfElms = 0;
+    carouselElms.forEach((elm) => {
+      elmsWidthSum += getElmWidth(elm);
+      noOfElms++;
+    });
+    return elmsWidthSum / noOfElms;
+  };
 
-// recipes of the month carousel
-// consst recipesOfTheMonth = document.querySelectorAll(".carousel-item");
-// recipesOfTheMonth.forEach(
-//   (recipe, i) => (recipe.style.transform = `translateX(${i * 110}%)`)
-// );
-// let currentItem = 0;
+  const carouselContainerWidth = getElmWidth(carouselContainer);
+  let distanceToTravel = getCarouselElmsWidthMean(carouselItemsClass) * 1.1;
 
-// const putCarouselItemsInPosition = function (itemsClass) {
-//   const carouselItems = document.querySelectorAll(`.${itemsClass}`);
-//   // if (currentItem === carouselItems.length - 2) currentItem = 0;
-//   // if (currentItem === 0) currentItem = carouselItems.length - 3;
-//   carouselItems.forEach((item, index) => {
-//     item.style.transform = `translateX(${(index - currentItem) * 110}%)`;
-//   });
-// };
+  const noOfVisibleCarouselElms = function () {
+    let result = carouselContainerWidth / distanceToTravel;
+    return Math.floor(result);
+  };
+
+  let currentItem = 0;
+  let carouselFullWidth;
+
+  const navigateCarousel = function () {
+    let sumOfPreviousElementsWidth = 0;
+    carouselItems.forEach((item) => {
+      const widthInPx = getElmWidth(item);
+      item.style.transform = `translateX(${
+        sumOfPreviousElementsWidth * 1.1 - currentItem * distanceToTravel
+      }px)`;
+      sumOfPreviousElementsWidth += widthInPx;
+    });
+    carouselFullWidth = sumOfPreviousElementsWidth * 1.1;
+  };
+
+  const carouselNextItem = function () {
+    currentItem++;
+    if (
+      carouselContainerWidth + (currentItem - 1) * distanceToTravel >=
+      carouselFullWidth
+    )
+      currentItem = 0;
+    navigateCarousel();
+  };
+
+  const carouselPrevItem = function () {
+    currentItem--;
+    if (currentItem < 0)
+      currentItem = carouselItems.length - noOfVisibleCarouselElms();
+    navigateCarousel();
+  };
+
+  navigateCarousel();
+  nextBtn.addEventListener("click", carouselNextItem);
+  prevBtn.addEventListener("click", carouselPrevItem);
+};
+
+enableCarouselFunctionality(
+  "filter-carousel",
+  "cuisine-carousel-item",
+  "cuisine-carousel-left-arr",
+  "cuisine-carousel-right-arr"
+);
+enableCarouselFunctionality(
+  "filter-carousel",
+  "diet-carousel-item",
+  "diet-carousel-left-arr",
+  "diet-carousel-right-arr"
+);
+enableCarouselFunctionality(
+  "carousel",
+  `carousel-item`,
+  "arrow-left",
+  "arrow-right"
+);
 
 let searchResults = [];
 const correctUserInputText = function (str) {

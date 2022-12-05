19b6d91fb4b59f3d4d640687219d23c8a010feaf
webBasedBob <catalin.b.pop97@gmail.com>
2022-12-05 19:14:11 +0200

    added functionality to UI with JS - accounts settings section

--- accConsole.js
+++ accConsole.js
@@ -5,6 +5,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
+  signOut,
 } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 
 const firebaseConfig = {
@@ -21,19 +22,90 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
+let user;
 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
+onAuthStateChanged(auth, (curUser) => {
+  if (curUser) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
-    const uid = user.uid;
+    const uid = curUser.uid;
     console.log("pula mea, cica merge", uid);
+    // user.displayName = "bob";
+    console.log(curUser);
     // ...
+    showAccountConsole();
+    hideAuthModal();
+    user = auth.currentUser;
+    renderUserInfo();
   } else {
+    renderNotLoggedInScreen();
     // User is signed out
     // ...
   }
 });
+
+const renderUserInfo = function () {
+  const userName = document.querySelector(".acc-info-cur-data.name");
+  const userEmail = document.querySelector(".acc-info-cur-data.email");
+  const userPhone = document.querySelector(".acc-info-cur-data.phone");
+
+  userName.innerText = user.displayName || "-";
+  userEmail.innerText = user.email || "-";
+  userPhone.innerText = user.phoneNumber || "-";
+};
+
+const renderNotLoggedInScreen = function () {
+  const notLoggedInScreen = document.querySelector(".not-logged-in-screen");
+  notLoggedInScreen.classList.remove("hidden");
+};
+
+const showAuthModal = function () {
+  const overlay = document.querySelector(".overlay");
+  const authModal = document.querySelector(".auth-modal");
+
+  overlay.classList.remove("hidden");
+  authModal.classList.remove("hidden");
+};
+// signOut(auth)
+//   .then(() => {
+//     // Sign-out successful.
+//   })
+//   .catch((error) => {
+//     // An error happened.
+//   });
+
+const showAccountConsole = function () {
+  const accountConsole = document.querySelector(".console-wrapper");
+
+  accountConsole.classList.remove("hidden");
+};
+
+const hideAuthModal = function () {
+  const overlay = document.querySelector(".overlay");
+  const authModal = document.querySelector(".auth-modal");
+  const notLoggedInScreen = document.querySelector(".not-logged-in-screen");
+
+  overlay.classList.add("hidden");
+  authModal.classList.add("hidden");
+  notLoggedInScreen.classList.add("hidden");
+};
+
+const showExpandedAccSetting = function (e) {
+  if (e.target.localName !== "button") return;
+
+  const allExpandedAccSettings = document.querySelectorAll(
+    ".acc-expanded-setting"
+  );
+  const expandedAccSettingToShow = document.querySelector(
+    `.${e.target.dataset.elmToShowClass}`
+  );
+
+  allExpandedAccSettings.forEach((elm) => {
+    elm.classList.add("hidden");
+  });
+  expandedAccSettingToShow.classList.remove("hidden");
+};
+
 const switchAuthWindow = function () {
   const chooseSignUpBtn = document.querySelector(".choose-action-sign-up");
   const chooseLoginBtn = document.querySelector(".choose-action-login");
@@ -41,11 +113,15 @@ const switchAuthWindow = function () {
   const loginContainer = document.querySelector(".login-container");
 
   chooseSignUpBtn.addEventListener("click", function () {
+    chooseSignUpBtn.classList.add("btn-active");
+    chooseLoginBtn.classList.remove("btn-active");
     signUpContainer.classList.remove("hidden");
     loginContainer.classList.add("hidden");
   });
 
   chooseLoginBtn.addEventListener("click", function () {
+    chooseSignUpBtn.classList.remove("btn-active");
+    chooseLoginBtn.classList.add("btn-active");
     loginContainer.classList.remove("hidden");
     signUpContainer.classList.add("hidden");
   });
@@ -93,3 +169,13 @@ const logIn = function () {
   });
 };
 logIn();
+
+const addEventListeners = function () {
+  const authMeBtn = document.querySelector(".auth-me");
+  authMeBtn.addEventListener("click", showAuthModal);
+  const expandedSettingsContainer = document.querySelector(
+    ".acc-actions-container"
+  );
+  expandedSettingsContainer.addEventListener("click", showExpandedAccSetting);
+};
+addEventListeners();
